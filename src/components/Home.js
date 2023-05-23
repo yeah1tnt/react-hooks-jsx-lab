3132317af75aec3f5829714e77ec832e913d5073
Tong Tang <pichip22@hotmail.com>
2023-05-23 01:09:21 -0400

    completed

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,14 @@
 import React from "react";
 import { name, city } from "../data/data.js";
 
+const divStyle = {
+  color:'firebrick'
+};
 function Home() {
   // update the JSX being returned!
-  return <div>Home</div>;
+  return <div>
+    <h1 id="home" style={divStyle}>{name} is a Web Developer from {city}</h1>
+  </div>;
 }
 
 export default Home;
